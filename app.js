67bea0c1ef77773e8029e1b9f78f273d48c07319
matthewmjm <matthewmjm@yahoo.com>
2016-12-29 17:41:01 -0700

    completed drill

---
+++ app.js
@@ -0,0 +1,10 @@
+function handleBulbClicks() {
+    $('.lightbulb').click(function (event) {
+        $('.lightbulb').removeClass('bulb-on');
+        $(event.currentTarget).addClass('bulb-on')
+    });
+}
+
+$(function () {
+    handleBulbClicks();
+});
